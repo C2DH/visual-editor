96ac0cb4a9e991e9c89db0e27bd29aea105742e4
uf0 <scnepa222@gmail.com>
2019-04-24 15:30:51 +0200

    update bs and urls

--- src/state/saga/auth.js
+++ src/state/saga/auth.js
@@ -1,4 +1,4 @@
-import { take, call, put, select } from 'redux-saga/effects'
+import { take, call, put, select } from "redux-saga/effects";
 import {
   LOGIN,
   LOGIN_LOADING,
@@ -10,121 +10,148 @@ import {
   TOKEN_REFRESHED,
   LOGOUT,
   logout,
-  tokenRefreshed,
-} from '../actions'
+  tokenRefreshed
+} from "../actions";
 
-const makeAuth = ({
-  meCall,
-  loginCall,
-  refreshTokenCall,
-}) => {
-
+const makeAuth = ({ meCall, loginCall, refreshTokenCall }) => {
   // redux saga helpers for Local Storage
   // TODO: Sorround shit above in a try/catch prevent bugged behaviour on
   // mobile safari anonymous navigaton
-  const STORAGE_KEY_NS = 'www1Visualeditor'
-  function *lsGetAccessToken() {
-    return yield call([window.localStorage, 'getItem'], `${STORAGE_KEY_NS}:accessToken`)
+  const STORAGE_KEY_NS = "bglVisualeditor";
+  function* lsGetAccessToken() {
+    return yield call(
+      [window.localStorage, "getItem"],
+      `${STORAGE_KEY_NS}:accessToken`
+    );
   }
-  function *lsGetRefreshToken() {
-    return yield call([window.localStorage, 'getItem'], `${STORAGE_KEY_NS}:refreshToken`)
+  function* lsGetRefreshToken() {
+    return yield call(
+      [window.localStorage, "getItem"],
+      `${STORAGE_KEY_NS}:refreshToken`
+    );
   }
-  function *lsStoreAccessToken(token) {
-    return yield call([window.localStorage, 'setItem'], `${STORAGE_KEY_NS}:accessToken`, token)
+  function* lsStoreAccessToken(token) {
+    return yield call(
+      [window.localStorage, "setItem"],
+      `${STORAGE_KEY_NS}:accessToken`,
+      token
+    );
   }
-  function *lsStoreRefreshToken(token) {
-    return yield call([window.localStorage, 'setItem'], `${STORAGE_KEY_NS}:refreshToken`, token)
+  function* lsStoreRefreshToken(token) {
+    return yield call(
+      [window.localStorage, "setItem"],
+      `${STORAGE_KEY_NS}:refreshToken`,
+      token
+    );
   }
-  function *lsRemoveAccessToken(token) {
-    yield call([window.localStorage, 'removeItem'], `${STORAGE_KEY_NS}:accessToken`)
+  function* lsRemoveAccessToken(token) {
+    yield call(
+      [window.localStorage, "removeItem"],
+      `${STORAGE_KEY_NS}:accessToken`
+    );
   }
-  function *lsRemoveRefreshToken(token) {
-    yield call([window.localStorage, 'removeItem'], `${STORAGE_KEY_NS}:refreshToken`)
+  function* lsRemoveRefreshToken(token) {
+    yield call(
+      [window.localStorage, "removeItem"],
+      `${STORAGE_KEY_NS}:refreshToken`
+    );
   }
-  function *lsRemoveTokens() {
-    yield lsRemoveAccessToken()
-    yield lsRemoveRefreshToken()
+  function* lsRemoveTokens() {
+    yield lsRemoveAccessToken();
+    yield lsRemoveRefreshToken();
   }
 
   // redux saga helpers for getting tokens from redux store
-  function *getAccessToken() {
-    return yield select(state => state.auth.accessToken)
+  function* getAccessToken() {
+    return yield select(state => state.auth.accessToken);
   }
-  function *getRefreshToken() {
-    return yield select(state => state.auth.refreshToken)
+  function* getRefreshToken() {
+    return yield select(state => state.auth.refreshToken);
   }
 
   function* apiCallWithRefresh(accessToken, refreshToken, apiFn, ...args) {
     try {
-      const result = yield call(apiFn(accessToken), ...args)
+      const result = yield call(apiFn(accessToken), ...args);
       // Response ok no need to refresh
-      return [result, { accessToken, refreshToken }]
+      return [result, { accessToken, refreshToken }];
     } catch (error) {
       // refresh token and retry the call
       if (error.status === 401) {
-        let refresh
+        let refresh;
         try {
-          refresh = yield call(refreshTokenCall, refreshToken)
+          refresh = yield call(refreshTokenCall, refreshToken);
         } catch (_) {
           // Fuck off if the refresh call fails throw the original 401 error
-          throw error
+          throw error;
         }
-        const result = yield call(apiFn(refresh.access_token), ...args)
-        return [result, { accessToken: refresh.access_token, refreshToken: refresh.refresh_token }]
+        const result = yield call(apiFn(refresh.access_token), ...args);
+        return [
+          result,
+          {
+            accessToken: refresh.access_token,
+            refreshToken: refresh.refresh_token
+          }
+        ];
       } else {
         // Normal error handling
-        throw error
+        throw error;
       }
     }
   }
 
   // make an auth api call (curry given or taken from store accessToken)
   function* authApiCall(apiFn, ...args) {
-    const accessToken =  yield getAccessToken()
-    const refreshToken =  yield getRefreshToken()
+    const accessToken = yield getAccessToken();
+    const refreshToken = yield getRefreshToken();
 
     try {
-      const [result, refresh] = yield apiCallWithRefresh(accessToken, refreshToken, apiFn, ...args)
+      const [result, refresh] = yield apiCallWithRefresh(
+        accessToken,
+        refreshToken,
+        apiFn,
+        ...args
+      );
       if (refresh) {
-        yield lsStoreAccessToken(refresh.accessToken)
-        yield lsStoreRefreshToken(refresh.refreshToken)
-        yield put(tokenRefreshed(refresh))
+        yield lsStoreAccessToken(refresh.accessToken);
+        yield lsStoreRefreshToken(refresh.refreshToken);
+        yield put(tokenRefreshed(refresh));
       }
-      return result
+      return result;
     } catch (error) {
       if (error.status === 403 || error.status === 401) {
-        yield put(logout())
+        yield put(logout());
       }
-      throw error
+      throw error;
     }
   }
 
   function* authenticateWithStorageToken() {
     // Access toke from local storage
-    const lsAccessToken = yield lsGetAccessToken()
-    const lsRefreshToken = yield lsGetRefreshToken()
+    const lsAccessToken = yield lsGetAccessToken();
+    const lsRefreshToken = yield lsGetRefreshToken();
     if (lsAccessToken) {
-      yield put({ type: AUTH_WITH_TOKEN_LOADING })
+      yield put({ type: AUTH_WITH_TOKEN_LOADING });
       try {
         // Curried me
-        const me = token => () => meCall(token)
-        const [
-          user,
-          { accessToken, refreshToken }
-        ] = yield apiCallWithRefresh(lsAccessToken, lsRefreshToken, me)
+        const me = token => () => meCall(token);
+        const [user, { accessToken, refreshToken }] = yield apiCallWithRefresh(
+          lsAccessToken,
+          lsRefreshToken,
+          me
+        );
         // Save tokens and user info in redux store
         yield put({
           type: AUTH_WITH_TOKEN_SUCCESS,
           payload: { user, accessToken, refreshToken }
-        })
+        });
       } catch (error) {
         // TODO: Improve error message...
         yield put({
           type: AUTH_WITH_TOKEN_FAILURE,
-          error: error.message ? error.message : error,
-        })
+          error: error.message ? error.message : error
+        });
         // The token was wrong...
-        yield lsRemoveTokens()
+        yield lsRemoveTokens();
       }
     }
   }
@@ -132,19 +159,22 @@ const makeAuth = ({
   // Pull login action and then try to authenticate user with given
   // credentials, if ok store token.
   function* watchLogin() {
-    const { payload } = yield take(LOGIN)
-    const { credentials } = payload
-    yield put({ type: LOGIN_LOADING })
+    const { payload } = yield take(LOGIN);
+    const { credentials } = payload;
+    yield put({ type: LOGIN_LOADING });
     try {
-      const { access_token, refresh_token } = yield call(loginCall, credentials)
+      const { access_token, refresh_token } = yield call(
+        loginCall,
+        credentials
+      );
       // Using access token to get user info
-      const user = yield call(meCall, access_token)
+      const user = yield call(meCall, access_token);
       // The relevant is the refresh token
       try {
-        yield lsStoreAccessToken(access_token)
-        yield lsStoreRefreshToken(refresh_token)
+        yield lsStoreAccessToken(access_token);
+        yield lsStoreRefreshToken(refresh_token);
       } catch (error) {
-        console.error('Failed to store tokens to local storage', error)
+        console.error("Failed to store tokens to local storage", error);
       }
       // Notify redux store login is ok!
       yield put({
@@ -152,45 +182,45 @@ const makeAuth = ({
         payload: {
           user,
           accessToken: access_token,
-          refreshToken: refresh_token,
-        },
-      })
+          refreshToken: refresh_token
+        }
+      });
     } catch (error) {
       // TODO: Better error message...
       yield put({
         type: LOGIN_FAILURE,
-        error: !error ? null : error.message ? error.message : error,
-      })
+        error: !error ? null : error.message ? error.message : error
+      });
     }
   }
 
   // Pull logout action and then remove token from storage.
   function* watchLogout() {
-    yield take(LOGOUT)
-    yield lsRemoveTokens()
+    yield take(LOGOUT);
+    yield lsRemoveTokens();
   }
 
   // Auth flow saga
   function* authFlow() {
     // auth using local storage token
-    yield authenticateWithStorageToken()
+    yield authenticateWithStorageToken();
     if (yield getAccessToken()) {
-      yield watchLogout()
+      yield watchLogout();
     }
 
     // auth using login form
     while (true) {
-      yield watchLogin()
+      yield watchLogin();
       if (yield getAccessToken()) {
-        yield watchLogout()
+        yield watchLogout();
       }
     }
   }
 
   return {
     authFlow,
-    authApiCall,
-  }
-}
+    authApiCall
+  };
+};
 
-export default makeAuth
+export default makeAuth;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,21 @@
-import { reduce, isArray } from 'lodash'
+import { reduce, isArray } from "lodash";
 
-export const hexToRgb = (hexStr) => {
+export const hexToRgb = hexStr => {
   // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   const hex = hexStr.replace(shorthandRegex, (m, r, g, b) => {
-      return r + r + g + g + b + b
-  })
+    return r + r + g + g + b + b;
+  });
 
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  return result ? [
-    parseInt(result[1], 16),
-    parseInt(result[2], 16),
-    parseInt(result[3], 16)
-  ] : null
-}
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+      ]
+    : null;
+};
 
 // Make styles for form container
 export const makeContainerStyles = (
@@ -22,114 +24,124 @@ export const makeContainerStyles = (
   backgroundColorOverlay,
   backgroundColor,
   bbox,
-  token,
+  token
 ) => {
-  let themeContainerStyle = {}
-  let overlayStyle = {}
+  let themeContainerStyle = {};
+  let overlayStyle = {};
 
-  if (backgroundType === 'image') {
+  if (backgroundType === "image") {
     if (backgroundImage) {
-      const imageUrl = getBoundingBoxImage(token, backgroundImage, bbox)
-      themeContainerStyle = { backgroundImage: `url(${imageUrl})` }
+      const imageUrl = getBoundingBoxImage(token, backgroundImage, bbox);
+      themeContainerStyle = { backgroundImage: `url(${imageUrl})` };
     }
     if (backgroundColorOverlay) {
-      const rgb = hexToRgb(backgroundColorOverlay)
+      const rgb = hexToRgb(backgroundColorOverlay);
       if (rgb) {
-        const rgbColor = rgb.join(',')
-        const linearGradient =  `linear-gradient(rgb(${rgbColor}) 0%, rgba(${rgbColor}, 0.7) 5%,rgba(${rgbColor}, 0.7) 95%,rgb(${rgbColor}) 100%)`;
-        overlayStyle = { background: linearGradient }
+        const rgbColor = rgb.join(",");
+        const linearGradient = `linear-gradient(rgb(${rgbColor}) 0%, rgba(${rgbColor}, 0.7) 5%,rgba(${rgbColor}, 0.7) 95%,rgb(${rgbColor}) 100%)`;
+        overlayStyle = { background: linearGradient };
       }
     }
   } else {
     if (backgroundColor) {
-      themeContainerStyle = { backgroundColor }
+      themeContainerStyle = { backgroundColor };
     }
   }
 
-  return [ themeContainerStyle, overlayStyle ]
-}
+  return [themeContainerStyle, overlayStyle];
+};
 
-export const moveArrayAhead = (v, i) =>
-  [ ...v.slice(0, i), v[i + 1], v[i] , ...v.slice(i + 2) ]
+export const moveArrayAhead = (v, i) => [
+  ...v.slice(0, i),
+  v[i + 1],
+  v[i],
+  ...v.slice(i + 2)
+];
 
-export const moveArrayBack = (v, i) =>
-  [ ...v.slice(0, i - 1), v[i], v[i - 1], ...v.slice(i + 1)]
+export const moveArrayBack = (v, i) => [
+  ...v.slice(0, i - 1),
+  v[i],
+  v[i - 1],
+  ...v.slice(i + 1)
+];
 
 // And now who are you?
 export const mergeOrdererList = (ordered, all) => {
-  const r = []
-  const by = all.reduce((r, v) => ({ ...r, [v]: true }), {})
+  const r = [];
+  const by = all.reduce((r, v) => ({ ...r, [v]: true }), {});
   ordered.forEach(o => {
     if (by[o]) {
-       r.push(o)
-       by[o] = false
+      r.push(o);
+      by[o] = false;
     }
-  })
-  return r.concat(reduce(by, (r, v, k) => v ? r.concat(+k) : r, []))
-}
+  });
+  return r.concat(reduce(by, (r, v, k) => (v ? r.concat(+k) : r), []));
+};
 
 const PLACE_TYPE_ICONS = {
   bombing: {
-    class: 'iconmap-bombing',
-    content: ''
+    class: "iconmap-bombing",
+    content: ""
   },
   hospital: {
-    class: 'iconmap-hospital',
-    content: ''
+    class: "iconmap-hospital",
+    content: ""
   },
   shelter: {
-    class: 'iconmap-shelter',
-    content: ''
+    class: "iconmap-shelter",
+    content: ""
   },
-  'steel-plant': {
-    class: 'iconmap-steel-plant',
-    content: ''
+  "steel-plant": {
+    class: "iconmap-steel-plant",
+    content: ""
   },
   cemetery: {
-    class: 'iconmap-cemetery',
-    content: ''
+    class: "iconmap-cemetery",
+    content: ""
   },
   memorial: {
-    class: 'iconmap-memorial',
-    content: ''
+    class: "iconmap-memorial",
+    content: ""
   },
-  'railway-station': {
-    class: 'iconmap-railway-station',
-    content: ''
+  "railway-station": {
+    class: "iconmap-railway-station",
+    content: ""
   },
-  'administrative-building': {
-    class: 'iconmap-administrative-building',
-    content: ''
+  "administrative-building": {
+    class: "iconmap-administrative-building",
+    content: ""
   },
-  'army-camp': {
-    class: 'iconmap-army-camp',
-    content: ''
+  "army-camp": {
+    class: "iconmap-army-camp",
+    content: ""
   }
-}
+};
 
 export const getPlaceTypeIcon = placeType => {
-  const icon = PLACE_TYPE_ICONS[placeType]
-  if (typeof icon === 'undefined') {
+  const icon = PLACE_TYPE_ICONS[placeType];
+  if (typeof icon === "undefined") {
     return {
-      class: 'iconmap-others',
-      content: ''
-    }
+      class: "iconmap-others",
+      content: ""
+    };
   }
-  return icon
-}
+  return icon;
+};
 
 const appendBeforeExtension = (file, str) => {
-  const pieces = file.split('.')
-  return pieces.slice(0, -1).join('.') + `${str}.` + pieces.slice(-1)
-}
+  const pieces = file.split(".");
+  return pieces.slice(0, -1).join(".") + `${str}.` + pieces.slice(-1);
+};
 
 export const getBoundingBoxImage = (token, imageUrl, bbox) => {
   if (isArray(bbox) && bbox.length === 4 && token) {
-    const bboxUrl = appendBeforeExtension(imageUrl, `_c[${bbox.join(',')}]`)
+    const bboxUrl = appendBeforeExtension(imageUrl, `_c[${bbox.join(",")}]`);
     // TODO: Move hardcoded url away...
-    return `https://ww1.lu/services/images?url=${encodeURI(bboxUrl)}&token=${token}`
+    return `https://bgl.dhlab.lu/services/images?url=${encodeURI(
+      bboxUrl
+    )}&token=${token}`;
   }
-  return imageUrl
-}
+  return imageUrl;
+};
 
-export * from './modules'
+export * from "./modules";
