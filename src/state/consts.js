1fc4311b7ed35eb538fd664589d021f31958b5c1
Daniele Guido <gui.daniele@gmail.com>
2020-06-09 10:19:27 +0200

    get tag id from environment (or use default)

--- src/state/consts.js
+++ src/state/consts.js
@@ -1,6 +1,6 @@
-export const TAG_THEME = 1
-export const TAG_CHAPTER = 2
-export const TAG_EDUCATION = 3
+export const TAG_THEME = parseInt(process.env.REACT_APP_TAG_THEME_PK || 1, 10)
+export const TAG_CHAPTER = parseInt(process.env.REACT_APP_TAG_CHAPTER_PK || 2, 10)
+export const TAG_EDUCATION = parseInt(process.env.REACT_APP_TAG_EDUCATION_PK || 3, 10)
 
 export const DEFAULT_BG_COLOR = '#1E1E1E'
 export const DEFAULT_OVERLAY_COLOR = '#1E1E1E'
