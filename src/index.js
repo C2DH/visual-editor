be7f999e7d81bf5a903e9520ce4291113ca605e5
Daniele Guido <gui.daniele@gmail.com>
2021-03-03 14:59:07 +0100

    add a lot of console.info

--- src/index.js
+++ src/index.js
@@ -10,11 +10,25 @@ console.info('Version:',
   process.env.REACT_APP_VISUAL_EDITOR_GIT_BRANCH,
   process.env.REACT_APP_VISUAL_EDITOR_GIT_REVISION,
 );
+console.info('REACT_APP_EDITOR_BASENAME:', process.env.REACT_APP_EDITOR_BASENAME)
+console.info('REACT_APP_MILLER_CLIENT_ID:', process.env.REACT_APP_MILLER_CLIENT_ID)
+console.info('REACT_APP_DOCUMENT_SCHEMA:', process.env.REACT_APP_DOCUMENT_SCHEMA)
 
-if (process.env.NODE_ENV === 'development') {
-  console.info('REACT_APP_MILLER_CLIENT_ID', process.env.REACT_APP_MILLER_CLIENT_ID)
-  console.info('REACT_APP_DOCUMENT_SCHEMA', process.env.REACT_APP_DOCUMENT_SCHEMA)
-}
+console.info('REACT_APP_FRONTEND_URL:', process.env.REACT_APP_FRONTEND_URL)
+
+console.info('REACT_APP_TAG_THEME_PK:', process.env.REACT_APP_TAG_THEME_PK)
+console.info('REACT_APP_TAG_CHAPTER_PK:', process.env.REACT_APP_TAG_CHAPTER_PK)
+console.info('REACT_APP_TAG_EDUCATION_PK:', process.env.REACT_APP_TAG_EDUCATION_PK)
+console.info('REACT_APP_DOCUMENT_SCHEMA:', process.env.REACT_APP_DOCUMENT_SCHEMA)
+console.info('REACT_APP_LANGUAGES:', process.env.REACT_APP_LANGUAGES)
+console.info('REACT_APP_DEFAULT_LANGUAGE:', process.env.REACT_APP_DEFAULT_LANGUAGE)
+console.info('REACT_APP_MILLER_CLIENT_ID:', process.env.REACT_APP_MILLER_CLIENT_ID)
+
+console.info('REACT_APP_CONTENT_ACCEPTED_FILES:', process.env.REACT_APP_CONTENT_ACCEPTED_FILES)
+console.info('REACT_APP_CONTENT_MAX_SIZE:', process.env.REACT_APP_CONTENT_MAX_SIZE)
+console.info('REACT_APP_PREVIEW_ACCEPTED_FILES:', process.env.REACT_APP_PREVIEW_ACCEPTED_FILES)
+console.info('REACT_APP_PREVIEW_MAX_SIZE:', process.env.REACT_APP_PREVIEW_MAX_SIZE)
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
