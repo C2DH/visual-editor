0f8bab3bd5f0149b33393813056482f45df68c30
Daniele Guido <gui.daniele@gmail.com>
2022-03-16 16:54:55 +0100

    add setup proxy for /api/ and /o/ endpoints

---
+++ src/setupProxy.js
@@ -0,0 +1,21 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+const fs = require('fs')
+
+const target = process.env.REACT_APP_PROXY || 'http://localhost'
+
+fs.appendFile(
+  './setupProxy.log',
+  `${(new Date()).toISOString()} target=${target}\n`,
+  (err) => console.error(err)
+);
+
+module.exports = function(app) {
+  app.use('/api', createProxyMiddleware({
+    target,
+    changeOrigin: true,
+  }));
+  app.use('/o', createProxyMiddleware({
+    target,
+    changeOrigin: true,
+  }));
+};
