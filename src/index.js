2c95586221dd990b13bddad14981827d9f28d4df
Daniele Guido <gui.daniele@gmail.com>
2022-04-13 11:16:01 +0200

    display current access token in the console info

--- src/index.js
+++ src/index.js
@@ -29,6 +29,9 @@ console.info('REACT_APP_CONTENT_MAX_SIZE:', process.env.REACT_APP_CONTENT_MAX_SI
 console.info('REACT_APP_PREVIEW_ACCEPTED_FILES:', process.env.REACT_APP_PREVIEW_ACCEPTED_FILES)
 console.info('REACT_APP_PREVIEW_MAX_SIZE:', process.env.REACT_APP_PREVIEW_MAX_SIZE)
 
+console.info('REACT_APP_MABOX_ACCESS_TOKEN:', process.env.REACT_APP_MABOX_ACCESS_TOKEN)
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
