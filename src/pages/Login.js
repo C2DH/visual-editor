75e54d3799f782f811eede5382a0bf81eb81d81a
Daniele Guido <gui.daniele@gmail.com>
2022-11-09 07:20:39 +0100

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,48 @@
+import axios from 'axios'
 import { useState } from 'react'
 import { Button, Container, Row, Col, Form } from 'react-bootstrap'
-import { useQuery } from 'react-query'
+import { useMutation } from 'react-query'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { BootstrapColumnLayout } from '../constants'
 import { useSettingsStore, useStore } from '../store'
 
 const Login = () => {
-  const user = useStore((state) => state.user)
-  const [credentials, setCredentials] = useState(null)
+  const navigate = useNavigate()
+  const location = useLocation()
+  const from = location.state?.from?.pathname || '/'
+  const millerAuthToken = useSettingsStore((state) => state.millerAuthToken)
   const millerOAuthUrl = useSettingsStore((state) => state.millerOAuthUrl)
-  const { isLoading, error, data } = useQuery(
-    'repoData',
-    () => {
-      console.debug('[Login] @useQuery', credentials)
-      return fetch(millerOAuthUrl).then((res) => res.json())
-    },
-    { enabled: credentials !== null },
+  const millerClientId = useSettingsStore((state) => state.millerClientId)
+  const setMillerAuthToken = useSettingsStore(
+    (state) => state.setMillerAuthToken
   )
+
+  const { isLoading, isError, error, data, mutate } = useMutation({
+    mutationFn: (credentials) => {
+      console.debug('[Login] @useQuery', credentials)
+      return axios
+        .post(
+          millerOAuthUrl,
+          new URLSearchParams({
+            client_id: millerClientId,
+            grant_type: 'password',
+            ...credentials,
+          })
+        )
+        .then((res) => {
+          console.debug('[Login] @useQuery res:', res)
+          if (res.ok === false) {
+            console.error('[Login] @useQuery error:', res)
+            throw new Error(res.statusText)
+          }
+          return res
+        })
+    },
+    onSuccess: (res) => {
+      setMillerAuthToken(res.data)
+      navigate(from, { replace: true })
+    },
+  })
 
   console.debug(
     '[Login] \n - millerOAuthUrl:',
@@ -24,22 +50,19 @@ const Login = () => {
     '\n isLoading, error, data:',
     isLoading,
     error,
-    data,
+    data
   )
-  // const navigate = useNavigate()
-  const location = useLocation()
-
-  let from = location.state?.from?.pathname || '/'
 
   const handleSubmit = (event) => {
     event.preventDefault()
 
     let formData = new FormData(event.currentTarget)
-    let email = formData.get('email')
-    let pwd = formData.get('pwd')
-    setCredentials({
-      email,
-      pwd,
+    let username = formData.get('username')
+    let password = formData.get('pwd')
+    // validate somehow before sending
+    mutate({
+      username,
+      password,
     })
     // auth.signin(username, () => {
     //   // Send them back to the page they tried to visit when they were
@@ -52,7 +75,7 @@ const Login = () => {
     // });
   }
 
-  if (user) {
+  if (millerAuthToken) {
     return <div>Already logged in! Continue to {from}</div>
   }
 
@@ -62,29 +85,33 @@ const Login = () => {
         <Col {...BootstrapColumnLayout}>
           <p>You must log in to view the page at {from}</p>
           {error !== null && (
-            <p className="text-danger">
+            <p className='text-danger'>
               Error: {error.message}
               <br />
-              Probably this is due to `Cross-Origin Request Blocked: The Same Origin Policy
-              disallows reading the remote resource`
+              Probably this is due to `Cross-Origin Request Blocked: The Same
+              Origin Policy disallows reading the remote resource`
             </p>
           )}
           <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control name="email" type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
+            <Form.Group className='mb-3' controlId='email'>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                name='username'
+                type='text'
+                placeholder='Enter email'
+              />
+              <Form.Text className='text-muted'>
+                Ask someone for the credentials.
               </Form.Text>
             </Form.Group>
-            <Form.Group className="mb-3" controlId="pwd">
+            <Form.Group className='mb-3' controlId='pwd'>
               <Form.Label>Password</Form.Label>
-              <Form.Control name="pwd" type="password" placeholder="Password" />
+              <Form.Control name='pwd' type='password' placeholder='Password' />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
+            <Form.Group className='mb-3' controlId='formBasicCheckbox'>
+              <Form.Check type='checkbox' label='Check me out' />
             </Form.Group>
-            <Button variant="primary" type="submit">
+            <Button variant='primary' type='submit'>
               Submit
             </Button>
           </Form>
