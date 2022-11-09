3ca19a0f5c24262eaaae7900ce12c2f0100f3899
Daniele Guido <gui.daniele@gmail.com>
2022-11-09 07:19:57 +0100

    Update RequireAuth.js

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -1,20 +1,24 @@
 import { Navigate, useLocation } from 'react-router-dom'
-import App from '../App'
 import { LoginRoute } from '../constants'
-import { useStore } from '../store'
+import { useSettingsStore } from '../store'
 
 // Always provide language code to match App.js routing mechanism
 const RequireAuth = ({ children, languageCode = '' }) => {
   let location = useLocation()
-  const user = useStore((state) => state.user)
+  const millerAuthToken = useSettingsStore((state) => state.millerAuthToken)
 
-  if (!user) {
+  if (!millerAuthToken) {
+    console.debug('[RequireAuth] user not authenticated to view:', location)
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
     return (
-      <Navigate to={`/${languageCode}/${LoginRoute.path}`} state={{ from: location }} replace />
+      <Navigate
+        to={`/${languageCode}/${LoginRoute.path}`}
+        state={{ from: location }}
+        replace
+      />
     )
   }
   return children
