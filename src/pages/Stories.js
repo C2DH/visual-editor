5bbba03de520fd9367096636e9322c5c4b36096f
Daniele Guido <gui.daniele@gmail.com>
2022-11-09 17:25:33 +0100

    Update Stories.js

--- src/pages/Stories.js
+++ src/pages/Stories.js
@@ -1,10 +1,21 @@
+import { useStories } from '@c2dh/react-miller'
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import StoryItem from '../components/Story/StoryItem'
 import { BootstrapColumnLayout } from '../constants'
 
 const Stories = () => {
   const { t } = useTranslation()
+
+  const [{ count = -1, results = [] } = {}, { isLoading, isSuccess }] =
+    useStories({
+      params: {
+        limit: 60,
+      },
+    })
+
+  console.debug('[Stories] test', count)
   return (
     <Container>
       <Row>
@@ -12,6 +23,18 @@ const Stories = () => {
           <h1>{t('pagesStoriesTitle')}</h1>
         </Col>
       </Row>
+      {isLoading && <p>loading</p>}
+      {isSuccess && (
+        <Row>
+          <Col>
+            {results.map((d) => (
+              <div key={d.id}>
+                <StoryItem story={d} className='mb-5' />
+              </div>
+            ))}
+          </Col>
+        </Row>
+      )}
     </Container>
   )
 }
