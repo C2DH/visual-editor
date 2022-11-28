516f37923e20a0b4853ed4a1496bb23abe39f08f
Daniele Guido <gui.daniele@gmail.com>
2022-11-28 20:11:09 +0100

    eslint

--- src/constants.js
+++ src/constants.js
@@ -27,16 +27,12 @@ export const NotFoundRoute = {
 
 export const AllRoutes = [HomeRoute, AboutRoute, SettingsRoute]
 
-export const Languages = (
-  process.env.REACT_APP_LANGUAGES ?? 'en-GB,fr-FR,de-DE'
-).split(',')
+export const Languages = (process.env.REACT_APP_LANGUAGES ?? 'en-GB,fr-FR,de-DE').split(',')
 export const MillerLanguages = Languages.map((l) => l.split('-').join('_'))
 export const LanguageCodes = Languages.map((l) => l.split('-')[0])
 export const LanguageRoutePattern = `/:lang(${LanguageCodes.join('|')})`
 export const LanguagePathRegExp = new RegExp(`/(${LanguageCodes.join('|')})/`)
-export const LanguageRootPathRegExp = new RegExp(
-  `^/(${LanguageCodes.join('|')})/?$`
-)
+export const LanguageRootPathRegExp = new RegExp(`^/(${LanguageCodes.join('|')})/?$`)
 export const DefaultLanguage = process.env.REACT_APP_DEFAULT_LANGUAGE ?? 'en-GB'
 export const DefaultLanguageCode = DefaultLanguage.split('-')[0]
 export const MillerAPI = process.env.REACT_APP_MILLER_API ?? '/api'
@@ -44,3 +40,8 @@ export const MillerAPI = process.env.REACT_APP_MILLER_API ?? '/api'
 export const BootstrapColumnLayout = Object.freeze({
   md: { span: 10, offset: 0 },
 })
+
+export const ViewParamName = 'view'
+export const ViewParamValueBasic = 'basic'
+export const ViewParamValueDiff = 'diff'
+export const ViewParamValueForm = 'form'
