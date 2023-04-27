d2f6304240c0ae1ec96d5857020bde5989048922
Daniele Guido <gui.daniele@gmail.com>
2023-04-27 09:30:25 +0200

    enbrich menu and fix css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,29 +3,65 @@ import User from './User'
 import LangLink from './LangLink'
 import { Github } from 'lucide-react'
 
-import { DocsRoute, PagesRoute, SettingsRoute, StoriesRoute } from '../constants'
+import { AuthorsRoute, DocsRoute, PagesRoute, SettingsRoute, StoriesRoute } from '../constants'
 import { useTranslation } from 'react-i18next'
+import { useStore } from '../store'
+import './Menu.css'
 
 const Menu = ({ className = '', children, style }) => {
   const { t } = useTranslation()
+  const setModalCreateDocOpen = useStore((state) => state.setModalCreateDocOpen)
   return (
     <aside className={`Menu ${className}`} style={style}>
       <h1>
-        visual
+        <del>visual</del>
         <br />
         editor
       </h1>
+      <ul>
+        <li>
+          <LangLink to={StoriesRoute.to}>{t(StoriesRoute.label)}</LangLink>
+        </li>
+        <li>
+          └─&nbsp;
+          <a href="#" onClick={() => setModalCreateDocOpen(true)}>
+            new story
+          </a>
+        </li>
+        <li>
+          <LangLink to={DocsRoute.to}>{t(DocsRoute.label)}</LangLink>
+        </li>
+        <li>
+          └─&nbsp;
+          <a href="#" onClick={() => setModalCreateDocOpen(true)}>
+            new document
+          </a>
+        </li>
+        <li>
+          <LangLink to={AuthorsRoute.to}>{t(AuthorsRoute.label)}</LangLink>
+        </li>
+        <li>
+          └─&nbsp;
+          <a href="#" onClick={() => setModalCreateDocOpen(true)}>
+            new author
+          </a>
+        </li>
 
-      <LangLink to={StoriesRoute.to}>{t(StoriesRoute.label)}</LangLink>
-      <br />
-      <LangLink to={DocsRoute.to}>{t(DocsRoute.label)}</LangLink>
-      <br />
-      <br />
-      <LangLink to={SettingsRoute.to}>{t(SettingsRoute.label)}</LangLink>
-      <br />
+        <li>
+          <LangLink to={PagesRoute.to}>{t(PagesRoute.label)}</LangLink>
+        </li>
+        <li>
+          └─&nbsp;
+          <a href="#" onClick={() => setModalCreateDocOpen(true)}>
+            new static page
+          </a>
+        </li>
+        <li>&nbsp;</li>
 
-      <LangLink to={PagesRoute.to}>{t(PagesRoute.label)}</LangLink>
-      <br />
+        <li>
+          <LangLink to={SettingsRoute.to}>{t(SettingsRoute.label)}</LangLink>
+        </li>
+      </ul>
       <section className="mt-3">
         <User></User>
       </section>
